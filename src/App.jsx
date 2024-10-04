8c0412ef7d194498bd7b6f29281ad0bd6b81b166
Aniket Singh <aniketsingh90126@gmail.com>
2024-10-04 15:09:16 +0530

    random color done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import RandomColor from "./Components/Random-color";
+
+const App = () => {
+  return (
+    <div>
+      <RandomColor />
+    </div>
+  );
+};
+
+export default App;
